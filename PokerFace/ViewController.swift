87c5453c0ca31e94418c654967bb23c935a14125
David Garcia <dgarcia@teravisiontech.com>
2018-01-08 12:00:40 -0400

    Initial Commit

---
+++ PokerFace/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PokerFace
+//
+//  Created by David Garcia on 1/8/18.
+//  Copyright © 2018 David Garcia. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
