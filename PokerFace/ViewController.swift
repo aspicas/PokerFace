296f733f8f30a26be1f0a60c3f77670dd73780af
David Garcia <david.a.g.r93@gmail.com>
2018-01-08 13:35:50 -0400

    Configured the view

--- PokerFace/ViewController.swift
+++ PokerFace/ViewController.swift
@@ -10,6 +10,12 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    override var prefersStatusBarHidden: Bool {
+        return true
+    }
+    
+    @IBOutlet weak var imageView: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
