ef59d3edd85e0245f0ef24b13b468fc29e8229c8
David Garcia <david.a.g.r93@gmail.com>
2018-01-08 14:03:34 -0400

    Select image

--- PokerFace/ViewController.swift
+++ PokerFace/ViewController.swift
@@ -8,7 +8,10 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController,
+                      UIImagePickerControllerDelegate, //Conjunto de Metodos que se deben implementar para mostrar la seleccion de imagenes.
+                      UINavigationControllerDelegate //Modificar el comportamiento cuando un viewController se muestra en una pila de navegacion
+                      {
 
     override var prefersStatusBarHidden: Bool {
         return true
@@ -16,9 +19,11 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var imageView: UIImageView!
     
+    var inputImage: UIImage?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        self.navigationItem.leftBarButtonItem = UIBarButtonItem(barButtonSystemItem: .camera, target: self, action: #selector(takePhoto))
     }
 
     override func didReceiveMemoryWarning() {
@@ -26,6 +31,25 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    @objc func takePhoto(){
+        let picker = UIImagePickerController()
+        picker.allowsEditing = true
+        picker.delegate = self
+        
+        present(picker, animated: true)
+    }
+    
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        guard let pickedImage = info[UIImagePickerControllerEditedImage] as? UIImage else { return }
+        self.inputImage = pickedImage
+        self.imageView.image = inputImage
+        dismiss(animated: true) {
+            //Detectar la cara de los usuarios 
+        }
+    }
 }
 
